bdb82af242499d56f9e17768d0642855047e041b
Taeyoon Lee <132141316+taeyoonL@users.noreply.github.com>
2023-07-26 23:45:27 +0900

    Update ViewController.swift

--- my_practice_7/ViewController.swift
+++ my_practice_7/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var page_controller: UIPageControl!
+    var texts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        page_controller.numberOfPages = 10
+        page_controller.currentPage = 0
+        page_controller.pageIndicatorTintColor = UIColor.black
+        page_controller.currentPageIndicatorTintColor = UIColor.red
+        label.text = "1"
+        
     }
 
-
+    @IBAction func page_control(_ sender: UIPageControl) {
+        label.text = texts[page_controller.currentPage]
+    }
+    
 }
 
